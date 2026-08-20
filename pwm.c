496057bd0a4b0a05670404a0bd2d261383296757
Roopeshwar Megadula <roopesh.megadula@gmail.com>
2026-08-20 22:44:45 +0530

    Update pwm.c

--- pwm.c
+++ pwm.c
@@ -1,18 +1,57 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "inc/hw_memmap.h"
+#include "inc/hw_types.h"
+#include "inc/hw_gpio.h"
+
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/pin_map.h"
+
 #include "pwm_control.h"
 
 volatile uint32_t ui32DutyCycle = 1;
 
-void PWM_SystemInit(void)
+void PWM_Init(void)
 {
-    // Clock configuration
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
 
-    // GPIOF enable
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_PWM1));
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF));
 
-    // PWM1 enable
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= GPIO_PIN_0;
 
-    // PF1 -> M1PWM5
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_4);
 
-    // PWM Generator configuration
+    GPIOPadConfigSet(GPIO_PORTF_BASE,
+                     GPIO_PIN_0 | GPIO_PIN_4,
+                     GPIO_STRENGTH_2MA,
+                     GPIO_PIN_TYPE_STD_WPU);
 
-    // Enable PWM output
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+
+    GPIOPinConfigure(GPIO_PF1_M1PWM5);
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_1);
+
+    PWMGenConfigure(PWM1_BASE,
+                    PWM_GEN_2,
+                    PWM_GEN_MODE_DOWN);
+
+    PWMGenPeriodSet(PWM1_BASE,
+                    PWM_GEN_2,
+                    PWM_PERIOD);
+
+    PWMPulseWidthSet(PWM1_BASE,
+                     PWM_OUT_5,
+                     ui32DutyCycle);
+
+    PWMGenEnable(PWM1_BASE, PWM_GEN_2);
+
+    PWMOutputState(PWM1_BASE,
+                   PWM_OUT_5_BIT,
+                   true);
 }
