da81c65b703190305b600e0ee2a973879f652b18
Roopeshwar Megadula <roopesh.megadula@gmail.com>
2026-08-20 22:48:58 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,20 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#include "driverlib/sysctl.h"
+
 #include "pwm_control.h"
 #include "timer_handler.h"
 
 int main(void)
 {
-    PWM_SystemInit();
+    SysCtlClockSet(SYSCTL_SYSDIV_5 |
+                   SYSCTL_USE_PLL |
+                   SYSCTL_OSC_MAIN |
+                   SYSCTL_XTAL_16MHZ);
+
+    PWM_Init();
+
     Timer0_Init();
 
     while(1)
