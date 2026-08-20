5ef4afb93a6a7c52610221299c5a7afd950563e7
Roopeshwar Megadula <roopesh.megadula@gmail.com>
2026-08-20 22:41:47 +0530

    Create pwm.c

---
+++ pwm.c
@@ -0,0 +1,18 @@
+#include "pwm_control.h"
+
+volatile uint32_t ui32DutyCycle = 1;
+
+void PWM_SystemInit(void)
+{
+    // Clock configuration
+
+    // GPIOF enable
+
+    // PWM1 enable
+
+    // PF1 -> M1PWM5
+
+    // PWM Generator configuration
+
+    // Enable PWM output
+}
