93f58015ae52c7eac21d2bc751526081d7043248
Roopeshwar Megadula <roopesh.megadula@gmail.com>
2026-08-20 22:42:39 +0530

    Create Timer_handler.c

---
+++ Timer_handler.c
@@ -0,0 +1,15 @@
+#include "timer_handler.h"
+#include "pwm_control.h"
+
+void Timer0_Init(void)
+{
+    // Timer0 periodic mode
+    // Interrupt configuration
+}
+
+void Timer0A_Handler(void)
+{
+    // Read buttons
+    // Update duty cycle
+    // Update PWM pulse width
+}
